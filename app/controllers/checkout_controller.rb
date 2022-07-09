3a6d5310a6abab452baf032553c7f5b13b653e63
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-09 20:17:26 +0200

    Fix linter errors

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,7 @@
 class CheckoutController < ApplicationController
   def create
     product = Product.find(params[:id])
-    @session = Stripe::Checkout::Session.create({ 
+    @session = Stripe::Checkout::Session.create({
                                                   customer: current_user.stripe_customer_id,
                                                   payment_method_types: ['card'],
                                                   line_items: [{
@@ -17,18 +17,17 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    if params[:session_id].present? 
+    if params[:session_id].present?
       session[:cart] = [] # empty cart = empty array
-      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
       @session_with_expand.line_items.data.each do |line_item|
         product = Product.find_by(stripe_product_id: line_item.price.product)
         product.increment!(:sales_count)
       end
     else
-      redirect_to cancel_url, alert: "No info to display"
+      redirect_to cancel_url, alert: 'No info to display'
     end
   end
 
-  def cancel
-  end
+  def cancel; end
 end
