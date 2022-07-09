3016be99d946e82d538f270fca749e4f5a94fcf1
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-09 20:40:38 +0200

    Update sales_counter from webhooks

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -19,11 +19,11 @@ class CheckoutController < ApplicationController
   def success
     if params[:session_id].present?
       session[:cart] = [] # empty cart = empty array
-      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
-      @session_with_expand.line_items.data.each do |line_item|
-        product = Product.find_by(stripe_product_id: line_item.price.product)
-        product.increment!(:sales_count)
-      end
+      # @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
+      # @session_with_expand.line_items.data.each do |line_item|
+      #   product = Product.find_by(stripe_product_id: line_item.price.product)
+      #   product.increment!(:sales_count)
+      # end
     else
       redirect_to cancel_url, alert: 'No info to display'
     end
