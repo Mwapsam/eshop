dcbad1bbb81798c3fd1c97ab398ffc4d01e17d32
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-09 03:11:17 +0200

    Add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+    validates :name, :price, presence: true
+    validates :price, numericality: {greater_than: 0, less_than: 10000000}
+
+    def to_s
+        name
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 end
