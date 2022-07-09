efeb438716d9657a8900c410326964640292cede
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-09 19:10:55 +0200

    Generate transaction report

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,12 +1,11 @@
 class CheckoutController < ApplicationController
   def create
     product = Product.find(params[:id])
-    @session = Stripe::Checkout::Session.create({
+    @session = Stripe::Checkout::Session.create({ 
+                                                  customer: current_user.stripe_customer_id,
                                                   payment_method_types: ['card'],
                                                   line_items: [{
-                                                    name: product.name,
-                                                    amount: product.price,
-                                                    currency: 'usd',
+                                                    price: product.stripe_price_id,
                                                     quantity: 1
                                                   }],
                                                   allow_promotion_codes: true,
@@ -15,5 +14,20 @@ class CheckoutController < ApplicationController
                                                   cancel_url: cancel_url
                                                 })
     redirect_to @session.url, status: 303, allow_other_host: true
+  end
+
+  def success
+    if params[:session_id].present? 
+      session[:cart] = [] # empty cart = empty array
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+      @session_with_expand.line_items.data.each do |line_item|
+        @product = Product.find_by(stripe_product_id: line_item.price.product)
+      end
+    else
+      redirect_to cancel_url, alert: "No info to display"
+    end
+  end
+
+  def cancel
   end
 end
