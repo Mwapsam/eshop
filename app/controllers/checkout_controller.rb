87ed9240a365aab43f9a395729e60d6222d5d684
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-10 09:10:36 +0200

    Update webhooks

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -13,6 +13,17 @@ class CheckoutController < ApplicationController
   end
 
   def success
+    if params[:session_id].present? 
+      # session.delete(:cart)
+      session[:cart] = [] # empty cart = empty array
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+      @session_with_expand.line_items.data.each do |line_item|
+        product = Product.find_by(stripe_product_id: line_item.price.product)
+        product.update(quantity: product.quantity - line_item.quantity)
+      end
+    else
+      redirect_to cancel_url, alert: "No info to display"
+    end
   end
 
   def cancel; end
