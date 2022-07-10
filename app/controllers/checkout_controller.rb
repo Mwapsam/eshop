9ea39016545d04a5ff35ce9ca8043b7a3ec44635
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-10 09:03:37 +0200

    Create line_items from @cart

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,18 +1,14 @@
 class CheckoutController < ApplicationController
   def create
-    product = Product.find(params[:id])
     @session = Stripe::Checkout::Session.create({
-                                                  customer: current_user.stripe_customer_id,
-                                                  payment_method_types: ['card'],
-                                                  line_items: [{
-                                                    price: product.stripe_price_id,
-                                                    quantity: 1
-                                                  }],
-                                                  allow_promotion_codes: true,
-                                                  mode: 'payment',
-                                                  success_url: "#{success_url}?session_id={CHECKOUT_SESSION_ID}",
-                                                  cancel_url: cancel_url
-                                                })
+      customer: current_user.stripe_customer_id,
+      payment_method_types: ['card'],
+      line_items: @cart.collect { |item| item.to_builder.attributes! },
+      allow_promotion_codes: true,
+      mode: 'payment',
+      success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: cancel_url,
+    })
     redirect_to @session.url, status: 303, allow_other_host: true
   end
 
